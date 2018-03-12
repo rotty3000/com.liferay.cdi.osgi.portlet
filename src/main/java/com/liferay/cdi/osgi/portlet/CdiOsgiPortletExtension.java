ffc4f3280b397e241e0084c8ed2b1c112857e565
Raymond Auge <raymond.auge@liferay.com>
2018-03-12 15:57:23 -0400

    initial pluto way of doing it
    Signed-off-by: Raymond Auge <raymond.auge@liferay.com>

--- src/main/java/com/liferay/cdi/osgi/portlet/CdiOsgiPortletExtension.java
+++ src/main/java/com/liferay/cdi/osgi/portlet/CdiOsgiPortletExtension.java
@@ -3,6 +3,7 @@ package com.liferay.cdi.osgi.portlet;
 import java.util.Dictionary;
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.Set;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.stream.Collectors;
 
@@ -13,6 +14,9 @@ import javax.enterprise.event.Observes;
 import javax.enterprise.inject.spi.Extension;
 import javax.portlet.Portlet;
 
+import org.apache.pluto.container.PortletInvokerService;
+import org.apache.pluto.container.om.portlet.PortletDefinition;
+import org.apache.pluto.container.om.portlet.impl.ConfigurationHolder;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 import org.slf4j.Logger;
@@ -25,9 +29,58 @@ public class CdiOsgiPortletExtension implements Extension {
 	private List<Entry<Portlet, Dictionary<String, Object>>> portlets = new CopyOnWriteArrayList<>();
 	private List<ServiceRegistration<Portlet>> registrations = new CopyOnWriteArrayList<>();
 
+	private volatile BundleContext bc;
+
 	//
 	// TODO populate 'portlets' list
 	//
+
+	void buildPortlets(Set<Class<?>> classes) throws Exception {
+		try {
+			// scan for method annotations
+
+			ConfigurationHolder holder = new ConfigurationHolder();
+			//holder.scanMethodAnnotations(ctx);
+
+			// Read the annotated configuration
+
+			if (classes != null) {
+				holder.processConfigAnnotations(classes);
+			}
+
+			if (_log.isDebugEnabled()) {
+				_log.debug("CdiOsgiPortletExtension on {} for annotations {}", bc.getBundle(), classes);
+			}
+
+			holder.validate();
+
+			// Reconcile the bean config with the explicitly declared portlet configuration.
+
+			holder.reconcileBeanConfig();
+
+			// If portlets have been found in this servlet context, launch the portlet servlets
+
+			for (PortletDefinition pd : holder.getPad().getPortlets()) {
+				String pn = pd.getPortletName();
+				String mapping = PortletInvokerService.URIPREFIX + pn;
+				String servletName = pn + "_PS3";
+
+				if (_log.isDebugEnabled()) {
+					StringBuilder sb = new StringBuilder();
+					sb.append("Adding Portlet: ");
+					sb.append(pn);
+					sb.append(", servlet name: ").append(servletName);
+					sb.append(", mapping: ").append(mapping);
+					_log.debug(sb.toString());
+				}
+
+				// make portlet
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 	void applicationScopedInitialized(
 		@Observes @Initialized(ApplicationScoped.class) Object ignore, BundleContext bc) {
